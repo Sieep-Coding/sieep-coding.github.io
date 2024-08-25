8c9228544c849b47b949bdc7924772ca16e567b9
Sieep-Coding <nstamb6@gmail.com>
2024-08-25 14:11:44 -0400

    formatting

--- src/components/about_section/Education.tsx
+++ src/components/about_section/Education.tsx
@@ -8,28 +8,28 @@ interface EducationProps {
 export default function Education(props: EducationProps) {
   const { education } = props;
 
-  return education.length == 0 ? (
+  return education.length === 0 ? (
     <div></div>
   ) : (
-    <div className="flex flex-col space-y-4 lg:w-1/2 mx-4">
-      <h1 className="text-3xl font-bold">Education</h1>
+    <div className="flex flex-col space-y-6 lg:w-2/3 mx-auto px-4">
+      <h1 className="text-4xl font-bold mb-6">Education</h1>
       {education.map((edu, index) => (
-        <div className="flex flex-col space-y-2 relative" key={index}>
-          <div className="flex items-center space-x-2">
-            <i className="fas fa-graduation-cap text-2xl text-secondary dark:text-dk-secondary dark:hover:text-dk-accent hover:text-accent z-10"></i>
-            <h2 className="text-xl font-semibold">{edu.title}</h2>
-          </div>
-          <div className="relative left-10 w-full">
-            <p className="text-xl font-normal">{edu.date}</p>
-            <p className="text-xl font-normal">{edu.location}</p>
-            {edu.gpa && <p className="text-xl font-normal">GPA: {edu.gpa}</p>}
-            {edu.thesis && (
-              <p className="text-xl font-normal">Activities: {edu.thesis}</p>
-            )}
+        <div className="flex flex-col space-y-4 relative" key={index}>
+          <div className="flex items-start space-x-4">
+            <i className="fas fa-graduation-cap text-3xl text-secondary dark:text-dk-secondary hover:text-accent dark:hover:text-dk-accent"></i>
+            <div className="flex-1">
+              <h2 className="text-2xl font-semibold">{edu.title}</h2>
+              <p className="text-lg font-normal">{edu.date}</p>
+              <p className="text-lg font-normal">{edu.location}</p>
+              {edu.gpa && <p className="text-lg font-normal mt-2">GPA: {edu.gpa}</p>}
+              {edu.thesis && (
+                <p className="text-lg font-normal mt-2">Activities: {edu.thesis}</p>
+              )}
+            </div>
           </div>
 
           {index !== education.length - 1 && (
-            <div className="absolute top-4 left-3 h-full w-1 bg-secondary dark:bg-dk-secondary hover:bg-accent dark:hover:bg-dk-accent z-0"></div>
+            <div className="absolute top-0 left-3 h-full w-1 bg-secondary dark:bg-dk-secondary hover:bg-accent dark:hover:bg-dk-accent"></div>
           )}
         </div>
       ))}
