842d57a68411530a2fa4cf13884944705af8d2b6
Nick <167996316+Sieep-Coding@users.noreply.github.com>
2024-09-13 14:21:57 -0400

    Update About.tsx

--- src/components/about_section/About.tsx
+++ src/components/about_section/About.tsx
@@ -12,7 +12,7 @@ export default function About(props: AboutProps) {
 
   return (
     <div className="flex flex-col items-center h-full space-y-6 p-4 sm:p-6">
-      <div className="flex flex-col space-y-4 w-full sm:w-3/4 lg:w-2/3 mx-4 sm:mx-6 p-4 sm:p-6 rounded-lg shadow-lg transform transition-transform hover:scale-105">
+      <div className="flex flex-col space-y-4 w-full sm:w-3/4 lg:w-2/3 mx-4 sm:mx-6 p-4 sm:p-6 rounded-lg shadow-lg transform transition-transform">
         <h1 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-center tracking-wide">About Me</h1>
         <p className="text-base sm:text-xl font-light leading-relaxed text-justify">
           {about.description}
