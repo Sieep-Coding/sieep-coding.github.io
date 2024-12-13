f6d1cc8ee318e820f50180a5ff18b1dba1a3f996
Sieep-Coding <nstamb6@gmail.com>
2024-12-12 23:35:20 -0500

    mobile update fix

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -13,7 +13,6 @@ export default function Nav({ posts }) {
     { name: "About", href: "/#about" },
     { name: "Projects", href: "/#projects" },
     { name: "Blog", href: "/posts" },
-    // { name: "Support My Non-Profit", href: "https://accessgr.vercel.app/" },
     { name: "Contact", href: "/#contact" },
   ];
 
@@ -59,22 +58,29 @@ export default function Nav({ posts }) {
               <span className="hidden lg:inline-block font-medium">Hire Me</span>
               <span className="lg:hidden font-medium">Hire</span>
             </button>
+
+            {/* GitHub Button */}
             <button
               className="relative px-5 py-2 rounded text-secondary dark:text-dk-secondary border-secondary dark:border-dk-secondary hover:bg-secondary dark:hover:bg-dk-secondary hover:text-primary dark:hover:text-primary cursor-pointer transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
             >
-              <a href="https://github.com/Sieep-Coding/sieep-coding.github.io" className="flex items-center space-x-2">
+              <a
+                href="https://github.com/Sieep-Coding/sieep-coding.github.io"
+                className="flex items-center space-x-2"
+              >
                 <i className="fab fa-github text-3xl"></i>
-                {/* <span className="hidden lg:inline-block font-medium">Github Repo</span> */}
               </a>
             </button>
           </div>
 
+          {/* Mobile Hamburger Menu */}
           <div className="inline-flex lg:hidden text-secondary dark:text-dk-secondary">
             <Hamburger
               onClick={() => setIsNavOpen(!isNavOpen)}
               isNavOpen={isNavOpen}
             />
           </div>
+
+          {/* Desktop Links */}
           <div className="hidden lg:block">
             <ul className="inline-flex text-secondary dark:text-dk-secondary text-2xl font-normal">
               {navLinks.map((link, index) => (
@@ -94,13 +100,12 @@ export default function Nav({ posts }) {
             </ul>
           </div>
         </div>
+
+        {/* Mobile Menu Links */}
         <div
-          className={
-            !isNavOpen
-              ? "hidden"
-              : "" +
-              " h-full flex flex-col items-center text-center lg:hidden dark:text-tertiary"
-          }
+          className={`${
+            !isNavOpen ? "hidden" : ""
+          } h-full flex flex-col items-center text-center lg:hidden dark:text-tertiary`}
         >
           <ul className="w-full text-secondary dark:text-dk-secondary text-xl font-semibold">
             {navLinks.map((link, index) => (
@@ -117,12 +122,16 @@ export default function Nav({ posts }) {
           </ul>
         </div>
       </nav>
+
+      {/* Overlay for Mobile Menu */}
       {isNavOpen && (
         <div
           className="fixed inset-0 blur-3xl bg-black bg-opacity-50 z-40 lg:hidden"
           onClick={() => setIsNavOpen(false)}
         ></div>
       )}
+
+      {/* Hire Me Modal */}
       {isHireMeOpen && (
         <div
           className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-60"
