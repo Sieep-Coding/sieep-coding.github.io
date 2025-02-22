ec0d7d5254d990349c59d29f2fc0690a155534db
Sieep-Coding <nstamb6@gmail.com>
2025-02-22 16:23:15 -0500

    NEW BLOG POST 2-22-2025

--- src/components/about_section/About.tsx
+++ src/components/about_section/About.tsx
@@ -16,7 +16,7 @@ export default function About(props: AboutProps) {
     <div className="flex flex-col items-center h-full space-y-6 p-4 sm:p-6">
       <div className="flex flex-col space-y-4 w-full sm:w-3/4 lg:w-2/3">
       <h1 className="text-3xl sm:text-4xl font-bold mb-6 text-center">About Me</h1>
-        <p className="text-base sm:text-3xl leading-relaxed text-justify">
+        <p className="text-base sm:text-3xl leading-normal">
           {about.description}
         </p>
         <div>
@@ -24,11 +24,11 @@ export default function About(props: AboutProps) {
         </div>
       </div>
       <div className="w-full sm:w-3/4 lg:w-2/3">
-      <p className="text-base sm:text-3xl leading-relaxed text-justify">
+      <p className="text-base sm:text-3xl leading-normal">
         <Experience experience={about.experience} />
       </p>
       </div>
-      <p className="text-base sm:text-3xl leading-relaxed text-justify"></p>
+      <p className="text-base sm:text-3xl leading-normal"></p>
       <div className="w-full sm:w-3/4 lg:w-2/3">
         <Education education={about.education} />
       </div>
