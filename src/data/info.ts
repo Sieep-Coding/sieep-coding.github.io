924eba456a4d94798913b6954c0afc8e8dd38585
Sieep-Coding <nstamb6@gmail.com>
2025-03-27 19:27:44 -0400

    final update for the day

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,7 +1,7 @@
 export const info = {
   name: "Nick Stambaugh",
   brief_description:
-    "Full Stack Enterprise Application Engineer",
+    "Full Stack & Enterprise Application Engineer",
   role: "Full Stack Engineer",
   picture: "/img.jpg",
   picture_alt: "Nick Stambaugh",
