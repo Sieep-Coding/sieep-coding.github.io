b74b7426e8af48bad6203440c2ae9b740b7b006e
Sieep-Coding <nstamb6@gmail.com>
2025-03-29 09:50:09 -0400

    update packages, update mobile layout for blog posts, improve project card performance, new blog post

--- src/components/Pointer.jsx
+++ src/components/Pointer.jsx
@@ -1,160 +1,171 @@
-import React, { useRef, useEffect } from "react";
-import { useThemeColor } from "../hooks/useThemeColor";
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../tailwind.config.mjs";
+// import React, { useRef, useEffect } from "react";
+// import { useThemeColor } from "../hooks/useThemeColor";
+// import resolveConfig from "tailwindcss/resolveConfig";
+// import tailwindConfig from "../../tailwind.config.mjs";
 
-export default function Pointer() {
-  const canvasRef = useRef(null);
-  const twConfig = resolveConfig(tailwindConfig);
+// export default function Pointer() {
+//   const canvasRef = useRef(null);
+//   const twConfig = resolveConfig(tailwindConfig);
   
-  const themeColor = useThemeColor([
-    twConfig.theme.colors.accent,
-    twConfig.theme.colors.secondary,
-  ]);
+//   const themeColor = useThemeColor([
+//     twConfig.theme.colors.accent,
+//     twConfig.theme.colors.secondary,
+//   ]);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    let particles = [];
+//   useEffect(() => {
+//     const canvas = canvasRef.current;
+//     const ctx = canvas.getContext("2d");
+//     let particles = [];
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
 
-    const onResize = () => {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-    };
+//     const onResize = () => {
+//       canvas.width = window.innerWidth;
+//       canvas.height = window.innerHeight;
+//     };
 
-    window.addEventListener("resize", onResize);
+//     window.addEventListener("resize", onResize);
 
-    const mouse = {
-      x: undefined,
-      y: undefined,
-      last_x: undefined,
-      last_y: undefined,
-    };
+//     const mouse = {
+//       x: undefined,
+//       y: undefined,
+//       last_x: undefined,
+//       last_y: undefined,
+//     };
 
-    const onMouseMove = (e) => {
-      mouse.last_x = mouse.x;
-      mouse.last_y = mouse.y;
-      mouse.x = e.x;
-      mouse.y = e.y;
-    };
+//     let inactivityTimeout;
 
-    const onMouseOut = () => {
-      mouse.x = mouse.last_x;
-      mouse.y = mouse.last_y;
-    };
+// const onMouseMove = (e) => {
+//   clearTimeout(inactivityTimeout); // Reset the timeout each time the mouse moves
+//   mouse.last_x = mouse.x;
+//   mouse.last_y = mouse.y;
+//   mouse.x = e.x;
+//   mouse.y = e.y;
 
-    document.addEventListener("mousemove", onMouseMove);
-    document.addEventListener("mouseout", onMouseOut);
+//   // Set a new timeout to clear particles after 2 seconds of inactivity
+//   inactivityTimeout = setTimeout(() => {
+//     particles = [];
+//   }, 2000);
+// };
 
-    function animate() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      particles.forEach((particle, index) => {
-        if (particle.isDead()) {
-          particles.splice(index, 1);
-        } else {
-          particle.update();
-        }
-      });
-      requestAnimationFrame(animate);
-    }
+// const onMouseOut = () => {
+//   mouse.x = mouse.last_x;
+//   mouse.y = mouse.last_y;
+//   clearTimeout(inactivityTimeout); // Clear the timeout when the mouse leaves
+//   particles = []; // Optionally clear particles immediately on mouseout
+// };
 
-    animate();
+// document.addEventListener("mousemove", onMouseMove);
+// document.addEventListener("mouseout", onMouseOut);
 
-    const particlesConfig = {
-      radius_in: [1, 3], 
-      vx_in: [-1, 1], 
-      vy_in: [-1, 1],
-      spread: 12, 
-      life: 13, 
-      interval: 0.4,
-      threshold: 2,
-      derivative_ratio: 12,
-    };
 
-    const random = (min, max) => Math.random() * (max - min) + min;
+//     function animate() {
+//       ctx.clearRect(0, 0, canvas.width, canvas.height);
+//       particles.forEach((particle, index) => {
+//         if (particle.isDead()) {
+//           particles.splice(index, 1);
+//         } else {
+//           particle.update();
+//         }
+//       });
+//       requestAnimationFrame(animate);
+//     }
 
-    function createParticle() {
-      const radius = random(...particlesConfig.radius_in);
-      const x = mouse.x;
-      const y = mouse.y;
-      let vx = random(...particlesConfig.vx_in);
-      let vy = random(...particlesConfig.vy_in);
-      const color = themeColor;
-      const life = particlesConfig.life;
-      const spread = particlesConfig.spread;
-      const threshold = particlesConfig.threshold;
-      const derivative_ratio = particlesConfig.derivative_ratio;
+//     animate();
 
-      if (
-        Math.abs(mouse.x - mouse.last_x) < threshold &&
-        Math.abs(mouse.y - mouse.last_y) < threshold
-      ) {
-        setTimeout(createParticle, particlesConfig.interval);
-        return;
-      }
+//     const particlesConfig = {
+//       radius_in: [1, 3], 
+//       vx_in: [-1, 1], 
+//       vy_in: [-1, 1],
+//       spread: 5, 
+//       life: 3, 
+//       interval: 0.2,
+//       threshold: 1,
+//       derivative_ratio: 3,
+//     };
 
-      if (mouse.last_x && mouse.last_y) {
-        vx = (mouse.x - mouse.last_x) / derivative_ratio;
-        vy = (mouse.y - mouse.last_y) / derivative_ratio;
-      }
+//     const random = (min, max) => Math.random() * (max - min) + min;
 
-      particles.push(
-        new Particle(x, y, vx, vy, radius, color, life, spread, ctx)
-      );
+//     function createParticle() {
+//       const radius = random(...particlesConfig.radius_in);
+//       const x = mouse.x;
+//       const y = mouse.y;
+//       let vx = random(...particlesConfig.vx_in);
+//       let vy = random(...particlesConfig.vy_in);
+//       const color = themeColor;
+//       const life = particlesConfig.life;
+//       const spread = particlesConfig.spread;
+//       const threshold = particlesConfig.threshold;
+//       const derivative_ratio = particlesConfig.derivative_ratio;
 
-      setTimeout(createParticle, particlesConfig.interval);
-    }
+//       if (
+//         Math.abs(mouse.x - mouse.last_x) < threshold &&
+//         Math.abs(mouse.y - mouse.last_y) < threshold
+//       ) {
+//         setTimeout(createParticle, particlesConfig.interval);
+//         return;
+//       }
 
-    createParticle();
+//       if (mouse.last_x && mouse.last_y) {
+//         vx = (mouse.x - mouse.last_x) / derivative_ratio;
+//         vy = (mouse.y - mouse.last_y) / derivative_ratio;
+//       }
 
-    return () => {
-      window.removeEventListener("resize", onResize);
-      window.removeEventListener("mousemove", onMouseMove);
-      window.removeEventListener("mouseout", onMouseOut);
+//       particles.push(
+//         new Particle(x, y, vx, vy, radius, color, life, spread, ctx)
+//       );
 
-      particles = [];
-    };
-  }, [themeColor]);
+//       setTimeout(createParticle, particlesConfig.interval);
+//     }
 
-  return (
-    <canvas
-      ref={canvasRef}
-      className="fixed top-0 left-0 w-full h-full pointer-events-none z-50 hidden md:block not-sr-only"
-    ></canvas>
-  );
-}
+//     createParticle();
 
-function Particle(x, y, vx, vy, radius, color, life, spread, ctx) {
-  this.x = x + Math.random() * spread - spread / 2;
-  this.y = y + Math.random() * spread - spread / 2;
-  this.vx = vx;
-  this.vy = vy;
-  this.radius = radius;
-  this.color = color;
-  this.life = life;
-  this.ctx = ctx;
+//     return () => {
+//       window.removeEventListener("resize", onResize);
+//       window.removeEventListener("mousemove", onMouseMove);
+//       window.removeEventListener("mouseout", onMouseOut);
 
-  this.draw = function () {
-    this.ctx.beginPath();
-    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    this.ctx.fillStyle = this.color;
-    this.ctx.shadowColor = this.color;
-    this.ctx.shadowBlur = 10; // Reduced glow effect
-    this.ctx.fill();
-    this.ctx.closePath();
-  };
+//       particles = [];
+//     };
+//   }, [themeColor]);
 
-  this.update = function () {
-    this.x += this.vx;
-    this.y += this.vy;
-    this.life -= 1;
-    this.draw();
-  };
+//   return (
+//     <canvas
+//       ref={canvasRef}
+//       className="fixed top-0 left-0 w-full h-full pointer-events-none z-50 hidden md:block not-sr-only"
+//     ></canvas>
+//   );
+// }
 
-  this.isDead = function () {
-    return this.life <= 0;
-  };
-}
+// function Particle(x, y, vx, vy, radius, color, life, spread, ctx) {
+//   this.x = x + Math.random() * spread - spread / 2;
+//   this.y = y + Math.random() * spread - spread / 2;
+//   this.vx = vx;
+//   this.vy = vy;
+//   this.radius = radius;
+//   this.color = color;
+//   this.life = life;
+//   this.ctx = ctx;
+
+//   this.draw = function () {
+//     this.ctx.beginPath();
+//     this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+//     this.ctx.fillStyle = this.color;
+//     this.ctx.shadowColor = this.color;
+//     this.ctx.shadowBlur = 3; // Reduced glow effect
+//     this.ctx.fill();
+//     this.ctx.closePath();
+//   };
+
+//   this.update = function () {
+//     this.x += this.vx;
+//     this.y += this.vy;
+//     this.life -= 1;
+//     this.draw();
+//   };
+
+//   this.isDead = function () {
+//     return this.life <= 0;
+//   };
+// }
--- src/components/post_section/RecentBlogs.tsx
+++ src/components/post_section/RecentBlogs.tsx
@@ -20,34 +20,42 @@ const RecentPosts: React.FC<RecentPostsProps> = ({ posts = [] }) => {
     .sort((a, b) => new Date(b.frontmatter.date).getTime() - new Date(a.frontmatter.date).getTime())
     .slice(0, K);
 
-    return (
-        <div className="flex flex-col border-t border-b md:border md:rounded-lg lg:flex-row justify-evenly mt-8 p-6">
-          <div className="flex flex-col gap-8">
-            <h1 className="text-3xl font-bold dark:text-accent">Recent Posts</h1>
-            {recentPosts.map((post, index) => (
-              <div key={index} className="flex flex-col gap-2">
-                <h1 className="text-lg font-semibold">
-                  <a href={post.url} className="hover:underline primary dark:text-accent">{post.frontmatter.title}</a>
-                </h1>
-                <p className="text-text dark:text-dk-text">{post.frontmatter.description}</p>
-                <p className="text-gray-600 dark:text-gray-400 text-sm">{post.frontmatter.date}</p>
-                <a
-                  className="text-secondary dark:text-dk-secondary hover:text-accent dark:hover:text-dk-accent"
-                  href={post.url}
-                >
-                  Read more &rarr;
-                </a>
-                <div className="border-t border-gray-200 dark:border-dk-gray-800" />
-              </div>
-            ))}
-            <div className="text-right mt-1 text-secondary dark:text-dk-secondary">
-              <a className="hover:text-accent dark:hover:text-dk-accent" href="/posts">
-                Read more posts
-              </a>
-            </div>
+  return (
+    <div className="flex flex-col border-t border-b md:border md:rounded-lg lg:flex-row justify-evenly mt-8 p-6">
+      <div className="flex flex-col gap-8">
+        <h1 className="text-3xl font-bold dark:text-accent">Recent Posts</h1>
+        {recentPosts.map((post, index) => (
+          <div key={index} className="flex flex-col gap-2">
+            <h1 className="text-lg font-semibold">
+              <a href={post.url} className="hover:underline primary dark:text-accent">{post.frontmatter.title}</a>
+            </h1>
+            <p className="text-text dark:text-dk-text">{post.frontmatter.description}</p>
+            <p className="text-gray-600 dark:text-gray-400 text-sm">{post.frontmatter.date}</p>
+            <a
+              className="text-secondary dark:text-dk-secondary hover:text-accent dark:hover:text-dk-accent"
+              href={post.url}
+            >
+              Read more &rarr;
+            </a>
+            <p className="text-gray-600 dark:text-dk-accent text-sm">
+              {post.frontmatter.tags.map((tag, index) => (
+                <span key={index}>
+                  {tag}
+                  {index < post.frontmatter.tags.length - 1 && <span className="mx-1"> </span>}
+                </span>
+              ))}
+            </p>
+            <div className="border-t border-gray-200 dark:border-dk-gray-800" />
           </div>
+        ))}
+        <div className="text-right mt-1 text-secondary dark:text-dk-secondary">
+          <a className="hover:text-accent dark:hover:text-dk-accent" href="/posts">
+            Read more posts
+          </a>
         </div>
-      );
-    }      
+      </div>
+    </div>
+  );
+}
 
 export default RecentPosts;
