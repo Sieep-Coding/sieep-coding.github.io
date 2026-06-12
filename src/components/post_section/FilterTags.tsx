dede4acf2bf88f5aec45e40e644ba029171878b6
Sieep-Coding <nickstambaugh@proton.me>
2026-06-12 11:27:11 -0400

    fix footer

--- src/components/post_section/FilterTags.tsx
+++ src/components/post_section/FilterTags.tsx
@@ -9,12 +9,12 @@ const FilterTags: React.FC<FilterTagsProps> = ({ tags, onClick }) => {
             {tags.map((tag) => (
                 <button
                     key={tag}
-                    className="relative overflow-hidden rounded-full px-5 py-2 text-1xl font-semibold tracking-wide transition-all 
-                        bg-primary text-text dark:bg-dk-primary dark:text-dk-text 
-                        hover:bg-gradient-to-r hover:from-accent dark:hover:from-dk-accent dark:hover:to-dk-secondary 
-                        hover:text-secondary dark:hover:text-black 
-                        shadow-md dark:shadow-lg 
-                        hover:scale-105 hover:-rotate-1 duration-300"
+                    className="relative overflow-hidden rounded-full px-3 py-1.5 text-sm sm:px-5 sm:py-2 sm:text-base font-semibold tracking-wide transition-all 
+    bg-primary text-text dark:bg-dk-primary dark:text-dk-text 
+    hover:bg-gradient-to-r hover:from-accent dark:hover:from-dk-accent dark:hover:to-dk-secondary 
+    hover:text-secondary dark:hover:text-black 
+    shadow-md dark:shadow-lg 
+    hover:scale-105 hover:-rotate-1 duration-300"
                     onClick={() => onClick(tag)}
                 >
                     {tag}
