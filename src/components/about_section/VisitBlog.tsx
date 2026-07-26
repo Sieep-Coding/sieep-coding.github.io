793d65f792f9bda6d677a2562c44ab2d1382cafc
sieep-coding <nickstambaugh@proton.me>
2026-07-26 13:10:43 -0400

    more cleanup

--- src/components/about_section/VisitBlog.tsx
+++ src/components/about_section/VisitBlog.tsx
@@ -5,7 +5,7 @@ export default function ViewBlog() {
       href="https://nickstambaugh.dev/posts"
       className="inline-block mt-10"
     >
-        <h2 className="text-xl font-medium text-text hover:text-secondary dark:text-dk-text dark:hover:text-dk-secondary transition-colors duration-200">
+        <h2 className="text-xl items-center text-center font-medium text-text hover:text-secondary dark:text-dk-text dark:hover:text-dk-secondary transition-colors duration-200">
           Visit Blog →
         </h2>
     </a>
