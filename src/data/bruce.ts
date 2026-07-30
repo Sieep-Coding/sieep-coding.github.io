667b37da2b7e7ab3653854549f588c45b4e7badd
sieep-coding <nickstambaugh@proton.me>
2026-07-29 20:04:30 -0400

    try new backend with ts

---
+++ src/data/bruce.ts
@@ -0,0 +1,9 @@
+export const brucePhotos = [
+  { src: "/bruce/bruce1.jpg", caption: "Slow down Bruce!" },
+  { src: "/bruce/bruce2.jpg", caption: "Bruce giving kisses." },
+  { src: "/bruce/bruce3.jpg", caption: "Bruce, supervising." },
+  { src: "/bruce/bruce4.jpg", caption: "Bruce gets a big hug." },
+  { src: "/bruce/bruce5.jpg", caption: "Bruce, on guard." },
+  { src: "/bruce/bruce6.jpg", caption: "Bruce, on patrol." },
+  { src: "/bruce/bruce7.jpg", caption: "Bruce and Nick in nature." },
+];
